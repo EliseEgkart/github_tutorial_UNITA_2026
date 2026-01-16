c40c5c865312a2c617cef06f0fa76433e9718c8b
Hyeongjin Kim <ky942400@gmail.com>
2026-01-16 15:22:24 +0900

    test~!

--- src/practice.cpp
+++ src/practice.cpp
@@ -3,5 +3,7 @@
 int main() {
     std::cout << "1: my name is <your_name>" << std::endl;
     std::cout << "2: I like <something>" << std::endl;
+    std::cout << "3: Test Finish~~" << std::endl;
+
     return 0;
 }
